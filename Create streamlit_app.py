6064d9d84b64115b1e1b9eb0c1c31ec2d470c937
NARESHVARIKUTI <varikuti.naresh@kasmodigital.com>
2026-01-20 01:24:53 +0530

    Update Create streamlit_app.py
    Updated app title to reflect new diner name

--- Create streamlit_app.py
+++ Create streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+streamlit.title("My Parents New Healthy Diner")
+
 
 # Write directly to the app
 st.title(":cup_with_straw: customize your smoothie:cup_with_straw: ")
